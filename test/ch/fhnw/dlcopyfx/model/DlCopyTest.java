a7b6bf21fa97eb5a32e470b9547c605fc46f645c
Gyssim <simon.gysin@students.fhnw.ch>
2016-11-28 14:52:55 +0100

    added test directory structure

---
+++ test/ch/fhnw/dlcopyfx/model/DlCopyTest.java
@@ -0,0 +1,16 @@
+package ch.fhnw.dlcopyfx.model;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by user on 28.11.16.
+ */
+public class DlCopyTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+}

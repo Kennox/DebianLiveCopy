7580f60c3e60826609dc2381cee089aea57a0ab3
Gyssim <simon.gysin@students.fhnw.ch>
2016-11-29 09:45:04 +0100

    added sample test

--- test/ch/fhnw/dlcopyfx/model/DlCopyTest.java
+++ test/ch/fhnw/dlcopyfx/model/DlCopyTest.java
@@ -1,16 +1,21 @@
 package ch.fhnw.dlcopyfx.model;
 
 import org.junit.Before;
+import org.junit.Test;
 
-import static org.junit.Assert.*;
 
 /**
  * Created by user on 28.11.16.
  */
 public class DlCopyTest {
-    @Before
-    public void setUp() throws Exception {
+  @Before
+  public void setUp() throws Exception {
 
-    }
+  }
+
+  @Test
+  public void sampleTest() {
+
+  }
 
 }
